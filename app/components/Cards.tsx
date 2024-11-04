6a11ef5d506ec527e4203b8cca71c3e0b856c80e
Ashar Khan <153488715+Asharkhan468@users.noreply.github.com>
2024-11-03 22:56:54 -0800

    BOOKING CARDS ICONS & GAP [FIX]

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -16,17 +16,17 @@ const BookingCard: React.FC<BookingCardProps> = ({
 }) => {
   return (
     <div
-      className={`${bgColor} rounded-lg shadow-xl h-40 w-64 mx-auto transition duration-200 ease-in-out hover:shadow-xl flex flex-col justify-center items-center`}
+      className={`${bgColor} rounded-lg shadow-xl h-44 w-56 mx-auto transition duration-200 ease-in-out hover:shadow-xl flex flex-col justify-center items-center`}
     >
-      <h2 className="text-lg font-semibold text-black mb-2 text-center truncate">
+      <div className="flex items-center gap-4">
+      <h2 className="text-lg font-semibold text-black mb-2 text-center truncate mt-2">
         {title}
       </h2>
-      <div className="flex items-center">
-        <p className="text-4xl font-bold text-black mr-4">{totalBookings}</p>
-        <div className="bg-white rounded-lg p-2 shadow-md flex items-center justify-center h-12 w-12">
-          <FontAwesomeIcon icon={icons} className="text-blue-600 text-2xl" />
+      <div className="bg-white rounded-lg p-2 shadow-md flex items-center justify-center h-7 w-7">
+          <FontAwesomeIcon icon={icons} className="text-blue-600 text-lg" />
         </div>
       </div>
+        <p className="text-4xl font-bold text-black mr-4">{totalBookings}</p>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -11,31 +11,40 @@ import { ComponentAreaChart } from "./components/BookingAreaChart";
 const page = () => {
   return (
     <>
-      <div className="flex items-center justify-center mt-12 bg-gray-50 flex-wrap gap-1 px-6">
+      <div className="flex items-center justify-center mt-12 flex-wrap gap-5 px-6">
+        <div className="mt-4">
         <BookingCard
           title="Total Booking"
           totalBookings={10}
           icons={faEnvelope}
           bgColor="bg-[#C8E7F8]"
+          
         />
-        <BookingCard
+        </div>
+       <div className="mt-4">
+       <BookingCard
           title="Total Income"
           totalBookings={18}
           icons={faMoneyBill}
           bgColor="bg-[#BBD4E2]"
         />
-        <BookingCard
+       </div>
+       <div className="mt-4">
+       <BookingCard
           title="Total Users"
           totalBookings={18}
           icons={faUser}
           bgColor="bg-[#C8E7F8]"
         />
+       </div>
+        <div className="mt-4">
         <BookingCard
           title="Total Provider"
           totalBookings={18}
           icons={faUser}
           bgColor="bg-[#BBD4E2]"
         />
+        </div>
       </div>
 
       <div className="bg-white m-4 p-5 flex flex-col gap-3 mt-8 my-4">
