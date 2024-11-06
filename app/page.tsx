4abadac568c0422d41c9f0782c9136eee414b00c
Ashar Khan <153488715+Asharkhan468@users.noreply.github.com>
2024-11-06 23:26:31 +0500

    CATEGORIES CARD [FIX] & BOOKINGS [ADD]

--- app/page.tsx
+++ app/page.tsx
@@ -11,85 +11,40 @@ import { ComponentAreaChart } from "./components/BookingAreaChart";
 const page = () => {
   return (
     <>
-    
-
-
-{/* <div className="bg-white m-4 p-1 flex flex-wrap justify-start gap-4 mt-6 my-2">
-  <div className="mt-1 w-full sm:w-[48%] lg:w-[28%] xl:w-[22%]">
-    <BookingCard
-      title="Total Booking"
-      totalBookings={10}
-      icons={faEnvelope}
-      bgColor="bg-[#C8E7F8]"
-    />
-  </div>
-  <div className="mt-1 w-full sm:w-[48%] lg:w-[28%] xl:w-[22%]">
-    <BookingCard
-      title="Total Income"
-      totalBookings={18}
-      icons={faMoneyBill}
-      bgColor="bg-[#BBD4E2]"
-    />
-  </div>
-  <div className="mt-1 w-full sm:w-[48%] lg:w-[28%] xl:w-[22%]">
-    <BookingCard
-      title="Total Users"
-      totalBookings={18}
-      icons={faUser}
-      bgColor="bg-[#C8E7F8]"
-    />
-  </div>
-  <div className="mt-1 w-full sm:w-[48%] lg:w-[28%] xl:w-[22%]">
-    <BookingCard
-      title="Total Provider"
-      totalBookings={18}
-      icons={faUser}
-      bgColor="bg-[#BBD4E2]"
-    />
-  </div>
-</div> */}
-
-
-
-
-<div className="bg-white m-2 p-2 flex flex-wrap justify-start gap-2 lg:gap-3 xl:gap-6 mt-4 my-4">
-  <div className="mt-1 w-full sm:w-[48%] lg:w-[23%] xl:w-[20%]">
-    <BookingCard
-      title="Total Booking"
-      totalBookings={10}
-      icons={faEnvelope}
-      bgColor="bg-[#C8E7F8]"
-    />
-  </div>
-  <div className="mt-1 w-full sm:w-[48%] lg:w-[23%] xl:w-[20%] ">
-    <BookingCard
-      title="Total Income"
-      totalBookings={18}
-      icons={faMoneyBill}
-      bgColor="bg-[#BBD4E2]"
-    />
-  </div>
-  <div className="mt-1 w-full sm:w-[48%] lg:w-[23%] xl:w-[20%]">
-    <BookingCard
-      title="Total Users"
-      totalBookings={18}
-      icons={faUser}
-      bgColor="bg-[#C8E7F8]"
-    />
-  </div>
-  <div className="mt-1 w-full sm:w-[48%] lg:w-[23%] xl:w-[20%]">
-    <BookingCard
-      title="Total Provider"
-      totalBookings={18}
-      icons={faUser}
-      bgColor="bg-[#BBD4E2]"
-    />
-  </div>
-</div>
-
-
-
-
+      <div className="bg-white m-4 p-1 flex flex-wrap justify-start gap-4 mt-6 my-2">
+        <div className="mt-1 w-full sm:w-[48%] lg:w-[28%] xl:w-[22%]">
+          <BookingCard
+            title="Total Booking"
+            totalBookings={10}
+            icons={faEnvelope}
+            bgColor="bg-[#C8E7F8]"
+          />
+        </div>
+        <div className="mt-1 w-full sm:w-[48%] lg:w-[28%] xl:w-[22%]">
+          <BookingCard
+            title="Total Income"
+            totalBookings={18}
+            icons={faMoneyBill}
+            bgColor="bg-[#BBD4E2]"
+          />
+        </div>
+        <div className="mt-1 w-full sm:w-[48%] lg:w-[28%] xl:w-[22%]">
+          <BookingCard
+            title="Total Users"
+            totalBookings={18}
+            icons={faUser}
+            bgColor="bg-[#C8E7F8]"
+          />
+        </div>
+        <div className="mt-1 w-full sm:w-[48%] lg:w-[28%] xl:w-[22%]">
+          <BookingCard
+            title="Total Provider"
+            totalBookings={18}
+            icons={faUser}
+            bgColor="bg-[#BBD4E2]"
+          />
+        </div>
+      </div>
 
       <div className="bg-white m-4 p-1 flex flex-col gap-3 mt-16 my-4 h-[315px]">
         <p className="text-2xl font-semibold text-left md:text-3xl lg:text-2xl">
