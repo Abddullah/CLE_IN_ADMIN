475a274aec50f1a3df685fa923f48387b62d7b66
Ashar Khan <153488715+Asharkhan468@users.noreply.github.com>
2024-12-03 12:51:45 +0500

    ALL ISSUES FIXED DONE

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig:NextConfig = {
     eslint:{
-        ignoreDuringBuilds: true
+        ignoreDuringBuilds: true,
     }
    
 };
