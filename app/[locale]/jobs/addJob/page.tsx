f65f0a122ea28b4b0de8b9c5440f444960693e2b
Ashar Khan <153488715+Asharkhan468@users.noreply.github.com>
2025-02-10 16:13:01 +0500

    EDIT JOB ADMIN CRASHING [FIX]

--- app/[locale]/jobs/addJob/page.tsx
+++ app/[locale]/jobs/addJob/page.tsx
@@ -196,7 +196,7 @@ function page() {
     handleRoomSizeChange((editData as any).roomSize);
     handleNoOfRoomsChange((editData as any).roomsQty);
 
-    const additionalService = (editData as any).aditionalServices?.map((item: any) => item.title) || [];
+    const additionalService = (editData as any)?.aditionalServices?.map((item: any) => item.title) || [];
 setSelectedServices(additionalService);
 
 
