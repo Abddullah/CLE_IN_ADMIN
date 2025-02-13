2eabf99bbbd591cef95e2306d96987a380f193de
Ashar Khan <153488715+Asharkhan468@users.noreply.github.com>
2025-02-13 19:42:18 +0500

    SOME BUG [FIX]

--- app/[locale]/jobs/page.tsx
+++ app/[locale]/jobs/page.tsx
@@ -23,23 +23,20 @@ import BookingModal from "../components/jobsComponent/JobDetailsCard";
 import { useSelector } from "react-redux";
 
 function Page() {
-  const t = useTranslations("Jobs"); 
+  const t = useTranslations("Jobs");
   const searchData = useSelector((state: any) => state.search.search);
   const [jobs, setJobs] = useState<any>([]);
-  const [allJobs , setAllJobs] = useState<any>([]);
+  const [allJobs, setAllJobs] = useState<any>([]);
   const [editableJob, setEditableJob] = useState<null | any>(null); // Store the job being edited
   const [isModalOpen, setIsModalOpen] = useState(false); // Modal visibility
   const [categories, setCategories] = useState<any[]>([]);
   const modalRef = useRef<HTMLDivElement | null>(null);
-  const [selectedJob , setSelectedJob] = useState<null | any>(null);
-  const [detailModalOpen , SetDetailModalOpen] = useState<boolean>(false);
-  const [status , setStatus]= useState()
-  const [StatusJob , setStatusJob ] = useState();
+  const [selectedJob, setSelectedJob] = useState<null | any>(null);
+  const [detailModalOpen, SetDetailModalOpen] = useState<boolean>(false);
+  const [status, setStatus] = useState();
+  const [StatusJob, setStatusJob] = useState();
 
-  const router = useRouter()
-
-
-  
+  const router = useRouter();
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -74,8 +71,6 @@ function Page() {
         ...doc.data(),
       }));
 
-     
-      
       setJobs(jobList);
       setAllJobs(jobList);
     });
@@ -83,25 +78,20 @@ function Page() {
     return () => unsubscribe(); // Clean up the listener on unmount
   }, []);
 
-  
-
   useEffect(() => {
-      const trimmedSearchData = searchData.trim().toLowerCase();
-    
-      if (trimmedSearchData !== "") {
-        const filterData = allJobs.filter(
-          (data:any) =>
-            data.category &&
-            data.category.toLowerCase().includes(trimmedSearchData)
-        );
-        setJobs(filterData);
-      } else {
-        setJobs(allJobs);
-      }
-    }, [searchData, allJobs]);
-  
-  
-  
+    const trimmedSearchData = searchData.trim().toLowerCase();
+
+    if (trimmedSearchData !== "") {
+      const filterData = allJobs.filter(
+        (data: any) =>
+          data.category &&
+          data.category.toLowerCase().includes(trimmedSearchData)
+      );
+      setJobs(filterData);
+    } else {
+      setJobs(allJobs);
+    }
+  }, [searchData, allJobs]);
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -121,63 +111,52 @@ function Page() {
     fetchCategories();
   }, []);
 
- 
-
-
-  const fetchJobById = async (jobId:string) => {
+  const fetchJobById = async (jobId: string) => {
     try {
-        const jobsRef = collection(db, "jobs"); // 'jobs' collection ka reference
-        const q = query(jobsRef, where("jobId", "==", jobId)); // Query jobId ke equal data ke liye
-        const querySnapshot = await getDocs(q);
+      const jobsRef = collection(db, "jobs"); // 'jobs' collection ka reference
+      const q = query(jobsRef, where("jobId", "==", jobId)); // Query jobId ke equal data ke liye
+      const querySnapshot = await getDocs(q);
 
-        const jobData = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-        }));
+      const jobData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
 
-        localStorage.setItem("editJob", JSON.stringify(jobData))
+      localStorage.setItem("editJob", JSON.stringify(jobData));
 
-        if (jobData.length > 0) {
-            console.log("Job Found:", jobData[0]);
-            return jobData[0]; // Agar ek hi job return hoti ho
-        } else {
-            console.log("No job found with this ID.");
-            return null;
-        }
+      if (jobData.length > 0) {
+        console.log("Job Found:", jobData[0]);
+        return jobData[0]; // Agar ek hi job return hoti ho
+      } else {
+        console.log("No job found with this ID.");
+        return null;
+      }
     } catch (error) {
-        console.error("Error fetching job:", error);
-        throw error;
+      console.error("Error fetching job:", error);
+      throw error;
     }
-};
- 
+  };
 
-const handleEditClick = async (job:any) => {
-  console.log("Editing job ID:", job.jobId);
-  try {
+  window.onpopstate = function () {
+    localStorage.removeItem("editJob");
+  };
+
+  const handleEditClick = async (job: any) => {
+    try {
       const editableJob = await fetchJobById(job.jobId);
       if (editableJob) {
-          setEditableJob(editableJob);
-          
+        setEditableJob(editableJob);
       }
 
-      router.push("/jobs/addJob")
-
-    
-  } catch (error) {
+      router.push("/jobs/addJob");
+    } catch (error) {
       console.error("Failed to fetch the job:", error);
-  }
-};
-
-
-   
-
-
-
-
+    }
+  };
 
   //handle click on the job
 
-  const handleJobClick = (job:any) => {
+  const handleJobClick = (job: any) => {
     setSelectedJob(job);
     SetDetailModalOpen(true);
   };
@@ -186,7 +165,6 @@ const handleEditClick = async (job:any) => {
     try {
       // Get the document reference
       const jobDocRef = doc(db, "jobs", job.id);
-     
 
       // Delete the document
       await deleteDoc(jobDocRef);
@@ -197,24 +175,19 @@ const handleEditClick = async (job:any) => {
     }
   };
 
-  const handleStatusChange = (e:any) => {
+  const handleStatusChange = (e: any) => {
     setStatus(e.target.value);
-   
-    
   };
 
-  
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-  const closeModal = ()=>{
-    setIsModalOpen(false)
-  }
-
-  const handleStatusClick = async(job:any)=>{
-    setIsModalOpen(true)
+  const handleStatusClick = async (job: any) => {
+    setIsModalOpen(true);
     setStatusJob(job);
-    setStatus(job.addStatus)
-  }
-
+    setStatus(job.addStatus);
+  };
 
   const updateJobStatus = async () => {
     if ((StatusJob as any)?.jobId) {
@@ -225,12 +198,6 @@ const handleEditClick = async (job:any) => {
       closeModal();
     }
   };
-
-  
-  
-  
-
- 
 
   return (
     <div className="bg-[#F5F7FA] w-full h-full overflow-hidden overflow-y-auto max-h-screen">
@@ -245,101 +212,106 @@ const handleEditClick = async (job:any) => {
       <JobTab />
 
       <div className="flex flex-wrap justify-center gap-12 w-full px-4 sm:px-8 sm:justify-start md:px-14 md:justify-start lg:justify-start lg:px-10 mt-4">
-  {jobs.length === 0 ? (
-    <div className="flex items-center justify-center h-[60vh] w-full text-gray-500 text-xl font-semibold">
-     {(t('no_job_available'))}
-    </div>
-  ) : (
-    jobs.map((job: any) => (
-      <div className="w-[310px] mt-[40px]" key={job.id}>
-        <Card
-          price={` € ${job.totalPriceWithTax}`}
-          title={job.category || "No Title"}
-          time={`${moment(job.bookingStart).format("hh:mm A")} - ${moment(job.bookingEnd).format("hh:mm A")}`}
-          imageUrl={job.imageUrl || "/assets/servicesIcons/cardImage.svg"}
-          status={job.addStatus || "Inactive"}
-          statusTextColor={"yellow-400"}
-          detailOpen={() => handleJobClick(job)}
-          date={`Date: ${moment(job.bookingDate).isValid() ? moment(job.bookingDate).format("MMM -D -YYYY") : "Invalid Date"}`}
-          createdAt={moment(job.createdAt).fromNow()}
-          dotsIcon="/assets/categoriesIcons/dots.svg"
-          onEdit={() => handleEditClick(job)}
-          onDelete={() => handleDeleteClick(job)}
-          onStatus={ ()=>handleStatusClick(job)}
-        />
+        {jobs.length === 0 ? (
+          <div className="flex items-center justify-center h-[60vh] w-full text-gray-500 text-xl font-semibold">
+            {t("no_job_available")}
+          </div>
+        ) : (
+          jobs.map((job: any) => (
+            <div className="w-[310px] mt-[40px]" key={job.id}>
+              <Card
+                price={` € ${job.totalPriceWithTax}`}
+                title={job.category || "No Title"}
+                time={`${moment(job.bookingStart).format("hh:mm A")} - ${moment(
+                  job.bookingEnd
+                ).format("hh:mm A")}`}
+                imageUrl={job.imageUrl || "/assets/servicesIcons/cardImage.svg"}
+                status={job.addStatus || "Inactive"}
+                statusTextColor={"yellow-400"}
+                detailOpen={() => handleJobClick(job)}
+                date={`Date: ${
+                  moment(job.bookingDate).isValid()
+                    ? moment(job.bookingDate).format("MMM -D -YYYY")
+                    : "Invalid Date"
+                }`}
+                createdAt={moment(job.createdAt).fromNow()}
+                dotsIcon="/assets/categoriesIcons/dots.svg"
+                onEdit={() => handleEditClick(job)}
+                onDelete={() => handleDeleteClick(job)}
+                onStatus={() => handleStatusClick(job)}
+              />
+            </div>
+          ))
+        )}
       </div>
-    ))
-  )}
-</div>
 
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 h-full">
+          <div className="bg-white w-[400px] p-6 rounded-lg shadow-lg relative">
+            {/* Close Button */}
+            <button
+              className="absolute top-2 right-4 text-2xl text-gray-500 hover:text-gray-800"
+              onClick={closeModal}
+            >
+              &times;
+            </button>
 
-{
-   isModalOpen && (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 h-full">
-      <div className="bg-white w-[400px] p-6 rounded-lg shadow-lg relative">
-        {/* Close Button */}
-        <button
-          className="absolute top-2 right-4 text-2xl text-gray-500 hover:text-gray-800"
-          onClick={closeModal}
-        >
-          &times;
-        </button>
+            {/* Modal Title */}
+            <h2 className="text-2xl font-semibold text-center mb-4">
+              {t("modalTitle")}
+            </h2>
 
-        {/* Modal Title */}
-        <h2 className="text-2xl font-semibold text-center mb-4">
-          {(t('modalTitle'))}
-        </h2>
+            {/* Select Field */}
+            <div className="mt-4">
+              <label
+                htmlFor="status-select"
+                className="block text-lg font-medium mb-2"
+              >
+                {t("selectStatusLabel")}
+              </label>
+              <div className="relative">
+                <select
+                  id="status-select"
+                  value={status}
+                  onChange={handleStatusChange}
+                  className="w-full p-3 pr-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 appearance-none"
+                >
+                  <option value="active">Active</option>
+                  <option value="pending">Pending</option>
+                  <option value="moderate">Moderate</option>
+                </select>
+                <span className="absolute inset-y-0 right-3 flex items-center pointer-events-none text-xl">
+                  ▾
+                </span>
+              </div>
+            </div>
 
-        {/* Select Field */}
-        <div className="mt-4">
-  <label
-    htmlFor="status-select"
-    className="block text-lg font-medium mb-2"
-  >
-    {t("selectStatusLabel")}
-  </label>
-  <div className="relative">
-    <select
-      id="status-select"
-      value={status}
-      onChange={handleStatusChange}
-      className="w-full p-3 pr-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 appearance-none"
-    >
-      <option value="active">Active</option>
-      <option value="pending">Pending</option>
-      <option value="moderate">Moderate</option>
-    </select>
-    <span className="absolute inset-y-0 right-3 flex items-center pointer-events-none text-xl">
-    ▾
-    </span>
-  </div>
-</div>
-
-        {/* Action Buttons */}
-        <div className="flex justify-end space-x-4 mt-6">
-          <button
-            onClick={closeModal}
-            className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-lg"
-          >
-            {(t('cancel'))}
-          </button>
-          <button
-            onClick={updateJobStatus}
-            className="bg-[#00BFFF] hover:bg-[#00BFFF] text-white py-2 px-4 rounded-lg"
-          >
-            {(t('save'))}
-          </button>
+            {/* Action Buttons */}
+            <div className="flex justify-end space-x-4 mt-6">
+              <button
+                onClick={closeModal}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-lg"
+              >
+                {t("cancel")}
+              </button>
+              <button
+                onClick={updateJobStatus}
+                className="bg-[#00BFFF] hover:bg-[#00BFFF] text-white py-2 px-4 rounded-lg"
+              >
+                {t("save")}
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-   )
-}
-   
-
-
+      )}
 
       <div>
-      {detailModalOpen && <BookingModal bookingData={selectedJob} handleClose={()=>SetDetailModalOpen(false)} />}
+        {detailModalOpen && (
+          <BookingModal
+            bookingData={selectedJob}
+            handleClose={() => SetDetailModalOpen(false)}
+          />
+        )}
       </div>
     </div>
   );
